3247d9b2f3a71f183c44f03bdb373198b1b7a9bc
Ömer KURT <omerkurt000@gmail.com>
2023-12-20 03:56:33 +0300

    server

--- server.js
+++ server.js
@@ -3,4 +3,4 @@
 const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
 const addonInterface = require("./addon.js")
 serveHTTP(addonInterface, { port: process.env.PORT || 60880 })
-publishToCentral("https://my-addon.awesome/manifest.json")
+publishToCentral("h3a33eebd6e68-turkio.baby-beamup.club/manifest.json")
